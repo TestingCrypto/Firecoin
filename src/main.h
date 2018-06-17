0a14abfe626a6115caef5a3660cf104752607a38
WolfLee (TestofTime) <chrispekoske@hotmail.com>
2018-06-17 12:39:21 -0500

    Update main.h
    + DEFAULT_MAX_ORPHAN_BLOCKS

--- src/main.h
+++ src/main.h
@@ -32,6 +32,7 @@ static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
+static const unsigned int DEFAULT_MAX_ORPHAN_BLOCKS = 750;
 static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE = 1000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
